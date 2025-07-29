7e9ad5b28c6e900e4075797a90ffc01d6ea6a779
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-07-29 13:03:53 -0700

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // Registro de usuario
   @Post('register')
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
